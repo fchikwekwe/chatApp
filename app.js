54108c57436b6f375cde6e20771134652c7b58f7
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-01-29 20:04:20 -0800

    🤦🏾‍♀️ forgot to set port listener correctly

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.get('/', (req, res) => {
 });
 
 /** Port Listener */
-server.listen('3000', () => {
-    console.log('Server listening on Port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log('Server listening on ', PORT);
 });
